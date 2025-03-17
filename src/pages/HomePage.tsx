6c7e05237e6ceed8028ba8cf04f2b79b8fcb8488
Victoria.White <victoria91718@gmail.com>
2025-03-17 13:51:23 +0800

    updated homePage btn location

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,24 +11,24 @@ export default function HomePage() {
             </button>
           </Link>
           <Link to="/sustainable-energy">
-            <BubbleButton className="fixed top-10 right-10" text='永續能源' size='16em' font='3xl'/>
+            <BubbleButton className="fixed top-95 left-150" text='永續能源' size='14em' font='2xl'/>
           </Link>
-          <BubbleButton className="fixed top-50 right-80" text='電力交易平台' size='10em' font='xl'/>
+          <BubbleButton className="fixed top-90 right-100" text='電力交易平台' size='8em' font='xl'/>
           <Link to="/buy-power">
-            <BubbleButton className="fixed top-85 right-30" text='我要買電' size='12em' font='2xl'/>
+            <BubbleButton className="fixed top-110 right-40" text='我要買電' size='12em' font='2xl'/>
           </Link>
           <Link to="/common-question-answer">
-            <BubbleButton className="fixed top-138 right-55" text='常見 Q&A' size='10em' font='2xl'/>
+            <BubbleButton className="fixed top-145 left-95" text='常見 Q&A' size='8em' font='xl'/>
           </Link>
           <Link to="/green-energy-consultant">
-            <BubbleButton className="fixed top-10 left-10" text='綠電顧問' size='16em' font='3xl'/>
+            <BubbleButton className="fixed top-132 right-135" text='綠電顧問' size='12em' font='2xl'/>
           </Link>
-          <BubbleButton className="fixed top-50 left-80" text='投票參與市場' size='10em' font='xl'/>
+          <BubbleButton className="fixed top-70 right-20" text='投票參與市場' size='8em' font='xl'/>
           <Link to="/buy-power">
-            <BubbleButton className="fixed top-85 left-30" text='我要賣電' size='12em' font='2xl'/>
+            <BubbleButton className="fixed top-85 left-55" text='我要賣電' size='12em' font='2xl'/>
           </Link>
           <Link to="/lastest-news">
-            <BubbleButton className="fixed top-138 left-55" text='最新消息' size='10em' font='2xl'/>
+            <BubbleButton className="fixed top-132 left-15" text='最新消息' size='8em' font='xl'/>
           </Link>
         </div>
       </div>
